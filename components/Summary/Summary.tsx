e35d42477afe6a17f0a015af87725f85b306b36e
Hamid Farmani <hamidfarmani1@gmail.com>
2023-03-15 13:08:30 +0100

    style: add hero to summary

--- components/Summary/Summary.tsx
+++ components/Summary/Summary.tsx
@@ -1,33 +1,52 @@
-import { Button, Container, Group, Stack, Text, Title } from "@mantine/core";
-import { FileDownload } from "tabler-icons-react";
+import { Button, Container, Text, Title } from "@mantine/core";
+import { Dots } from "./Dots";
+import { useSummaryStyles } from "./useSummaryStyles";
 
 export function Summary() {
+  const { classes } = useSummaryStyles();
+
   return (
-    <Container>
-      <Stack>
-        <Text fz="lg">
-          Hi! My name is <Title>Hamid</Title>
-        </Text>
-        <Text>
-          I am a highly motivated software engineer with a strong track record
-          of delivering successful projects for different clients. I have a wide
-          range of programming skills and am proficient in Java programming
-          language. In addition, I have a strong understanding of databases and
-          recently added React to my skill set, allowing me to take on
-          full-stack responsibilities.
-        </Text>
-      </Stack>
-      <Group position="center">
-        <Button
-          variant="outline"
-          leftIcon={<FileDownload />}
-          component="a"
-          target="_blank"
-          href="https://drive.google.com/file/d/1EsrNKthQVzyzg6yppHQPiNO29B3xNhUb/view?usp=share_link"
-        >
-          Resume
-        </Button>
-      </Group>
+    <Container className={classes.wrapper} size={1400}>
+      <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+      <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+      <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
+      <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
+
+      <div className={classes.inner}>
+        <Title className={classes.title}>
+          I&apos;m{" "}
+          <Text component="span" className={classes.highlight} inherit>
+            Hamid
+          </Text>
+          , a Software Engineer!
+        </Title>
+
+        <Container p={0} size={600}>
+          <Text size="lg" color="dimmed" className={classes.description}>
+            This website is a space where I share my experiences, interests, and
+            personal projects. If you&apos;re interested in learning more about
+            me and my work, keep scrolling to explore my skills and projects.
+          </Text>
+        </Container>
+
+        <div className={classes.controls}>
+          <Button
+            className={classes.control}
+            size="lg"
+            variant="default"
+            color="gray"
+            mr="md"
+            component="a"
+            target="_blank"
+            href="https://drive.google.com/file/d/1EsrNKthQVzyzg6yppHQPiNO29B3xNhUb/view?usp=share_link"
+          >
+            Check my resume
+          </Button>
+          <Button className={classes.control} size="lg">
+            Purchase a license
+          </Button>
+        </div>
+      </div>
     </Container>
   );
 }
---
+++ components/Summary/useSummaryStyles.tsx
@@ -0,0 +1,91 @@
+import { createStyles, rem } from "@mantine/core";
+
+export const useSummaryStyles = createStyles((theme) => ({
+  wrapper: {
+    position: "relative",
+    paddingTop: rem(120),
+    paddingBottom: rem(80),
+
+    [theme.fn.smallerThan("sm")]: {
+      paddingTop: rem(80),
+      paddingBottom: rem(60),
+    },
+  },
+
+  inner: {
+    position: "relative",
+    zIndex: 1,
+  },
+
+  dots: {
+    position: "absolute",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[5]
+        : theme.colors.gray[1],
+
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  dotsLeft: {
+    left: 0,
+    top: 0,
+  },
+
+  title: {
+    textAlign: "center",
+    fontWeight: 800,
+    fontSize: rem(40),
+    letterSpacing: -1,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    marginBottom: theme.spacing.xs,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan("xs")]: {
+      fontSize: rem(28),
+      textAlign: "left",
+    },
+  },
+
+  highlight: {
+    color:
+      theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6],
+  },
+
+  description: {
+    textAlign: "center",
+
+    [theme.fn.smallerThan("xs")]: {
+      textAlign: "left",
+      fontSize: theme.fontSizes.md,
+    },
+  },
+
+  controls: {
+    marginTop: theme.spacing.lg,
+    display: "flex",
+    justifyContent: "center",
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+
+  control: {
+    "&:not(:first-of-type)": {
+      marginLeft: theme.spacing.md,
+    },
+
+    [theme.fn.smallerThan("xs")]: {
+      height: rem(42),
+      fontSize: theme.fontSizes.md,
+
+      "&:not(:first-of-type)": {
+        marginTop: theme.spacing.md,
+        marginLeft: 0,
+      },
+    },
+  },
+}));
